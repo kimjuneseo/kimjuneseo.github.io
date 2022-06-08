a4b5dc048f36d8057eb3bcd7b25603154752ceb2
kimjuneseo <june3929213512@gmail.com>
2022-06-08 20:53:35 +0900

    형이 만져줌

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,5 +4,18 @@ export default {
     description: 'A VitePress site',
     themeConfig: {
         logo: 'https://avatars.githubusercontent.com/u/70473267?v=4',
-    }
+        sidebar: [
+            {
+              text: 'SDHS',
+              collapsible: true,
+              items: [
+                { text: '0608', link: '/sdhs/0608'},
+                { text: '0609', link: '/sdhs/0609'},
+                { text : '우하하하하', items:[
+                    { text:'난준서' , link:'/sdhs/우하하하하하하/난준서'}   
+                ]}
+              ]
+            }
+        ]
+    },
 }
