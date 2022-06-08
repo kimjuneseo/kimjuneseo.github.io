6f4a458641184f1b243b1039bb5c0ff3347edf97
kimjuneseo <june3929213512@gmail.com>
2022-06-08 15:41:22 +0900

    설정파일 수정

---
+++ .vitepress/config.ts
@@ -0,0 +1,8 @@
+export default {
+    base: '/TIL/',
+    title: "김준서 블로그",
+    description: 'A VitePress site',
+    themeConfig: {
+        logo: 'https://avatars.githubusercontent.com/u/70473267?v=4',
+    }
+}
