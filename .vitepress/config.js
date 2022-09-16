dc76571446dd56043517cf85c4e6f837b8e0533a
kimjuneseo <june3929213512@gmail.com>
2022-09-16 16:23:23 +0900

    1

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -27,24 +27,9 @@ export default {
 	      },
 	    ],
 	    ["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-LPCXPEB16R');"]],
-	  ],
+    ],
 }
 
-// module.exports = {
-//   plugins: [
-//     // other plugins
-//     [
-//       'rss-feed',
-//       {
-//         username: 'Bougie',
-//         hostname: 'https://www.bougieblog.cn',
-//         selector: '.content__post', // extract content to content:encoded
-//         count: 10,
-//         filter: (page) => /^blog/.test(page.relativePath),
-//       },
-//     ],
-//   ],
-// }
 
 
 function getRecall(){
