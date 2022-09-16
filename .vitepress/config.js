788be86055e23b80c330926a4c923aeea5a5ae40
kimjuneseo <june3929213512@gmail.com>
2022-09-16 16:27:21 +0900

    head에 추가

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -19,13 +19,7 @@ export default {
         format:"cjs",
       }
     },head: [
-	    [
-	      "script",
-	      {
-	        async: true,
-	        src: "https://www.googletagmanager.com/gtag/js?id=G-LPCXPEB16R",
-	      },
-	    ],
+	    ["script", {async: true,src: "https://www.googletagmanager.com/gtag/js?id=G-LPCXPEB16R"},[]],
 	    ["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-LPCXPEB16R');"]],
     ],
 }
