c3bbc7c839fbaec4a62faa70532950954fa7b717
kimjuneseo <june3929213512@gmail.com>
2022-11-13 16:58:13 +0900

    1

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -18,6 +18,12 @@ export default {
       ["meta", {name :"naver-site-verification", content:"9e31da0325d0f3bd5804f51c5bcc357a4c79918c"}],
       ["script", {async: true,  src: "https://www.googletagmanager.com/gtag/js?id=G-H14R86J9MR",}],
       ["script", {}, "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}; gtag('js', new Date()); gtag('config', 'G-H14R86J9MR');"],
+      ["script", {}, `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-K88Z3FK');`],
+      
     ],
     // sitemap
     transformHtml: (_, id, { pageData }) => {
